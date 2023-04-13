289cb556bf6e22bbe0349abb7b7f963763739400
namiqmamedov <namigmm@code.edu.az>
2023-04-13 22:06:02 +0400

    Updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,12 @@ import {
 } from 'reactstrap'
 import {Link} from 'react-router-dom'
 import '../../styles/footer.css'
+import { BsInstagram } from 'react-icons/bs';
+import { ImFacebook } from 'react-icons/im';
+import { BsTwitter } from 'react-icons/bs';
+import { TfiYoutube } from 'react-icons/tfi';
+import { FaPinterestP } from 'react-icons/fa';
+
 
 
 const Footer = () => {
@@ -18,8 +24,9 @@ const Footer = () => {
         <Container>
           <Row>
             <div className="footer-main">
-              <div className="footer-router">
-                  <h2 className="router-title">Customer Care</h2>
+              <div className="router-wrapper">
+              <div className="router-index">
+                  <h2 className="router-title">CUSTOMER CARE</h2>
                   <div className="router-item">
                     <ul>
                       <li>
@@ -49,6 +56,101 @@ const Footer = () => {
                       </li>
                     </ul>
                   </div>
+              </div>
+              <div className="router-index">
+                  <h2 className="router-title">OUR COMPANY</h2>
+                  <div className="router-item">
+                    <ul>
+                      <li>
+                        <Link>
+                        CONTACT US
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        CALL NOW: 800 227 8437
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        FAQ
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        CHECK ORDER
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        BOOK AN APPOINTMENT
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+              </div>
+              <div className="router-index">
+                  <h2 className="router-title">LEGAL AREA</h2>
+                  <div className="router-item">
+                    <ul>
+                      <li>
+                        <Link>
+                        CONTACT US
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        CALL NOW: 800 227 8437
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        FAQ
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        CHECK ORDER
+                        </Link>
+                      </li>
+                      <li>
+                        <Link>
+                        BOOK AN APPOINTMENT
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+              </div>
+              </div>
+              <div className="social-link">
+                <h2 className='router-title'>FOLLOW US</h2>
+                <ul>
+                  <li>
+                    <a href="#" target='_blank'>
+                      <BsInstagram/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target='_blank'>
+                      <ImFacebook/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target='_blank'>
+                      <BsTwitter/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target='_blank'>
+                      <TfiYoutube/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target='_blank'>
+                      <FaPinterestP/>
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </Row>
